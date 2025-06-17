0aeeffe09eeb263d15f341b7162162b2a060c315
guillaumedmgl <guillaume.ia.dmgl@gmail.com>
2025-06-17 08:12:24 +0200

    Add files via upload

---
+++ script_fiches.js
@@ -0,0 +1,415 @@
+const fichesData = {
+  fiches: {
+    '1A': {
+      '1': {
+        'Maths': [
+          { titre: 'Calculs de base', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' },
+          { titre: 'Fonctions usuelles', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Physique': [
+          { titre: 'Cinématique', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '2': {
+        'Info': [
+          { titre: 'Algorithmes', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Chimie': [
+          { titre: 'Réactions chimiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '2A': {
+      '3': {
+        'Mécanique': [
+          { titre: 'Résistance des matériaux', visionner: 'https://drive.google.com/file/d/19i65g06_nhSZx5naNHAza6lH6VwfBbN2/preview', telecharger: 'https://drive.google.com/uc?export=download&id=19i65g06_nhSZx5naNHAza6lH6VwfBbN2' }
+        ],
+        'Maths': [
+          { titre: 'Intégrales', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '4': {
+        'Électrotechnique': [
+          { titre: 'Circuits RLC', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Informatique': [
+          { titre: 'Structures de données', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '3A': {
+      '5': {
+        'Électronique': [
+          { titre: 'Transistors', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Thermodynamique': [
+          { titre: 'Cycles thermodynamiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '6': {
+        'Management': [
+          { titre: 'Gestion de projet', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Innovation': [
+          { titre: 'Méthodes créatives', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '4A': {
+      '7': {
+        'Maths': [
+          { titre: 'Calculs de base', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' },
+          { titre: 'Fonctions usuelles', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Physique': [
+          { titre: 'Cinématique', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '8': {
+        'Info': [
+          { titre: 'Algorithmes', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Chimie': [
+          { titre: 'Réactions chimiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '5A': {
+      '9': {
+        'Maths': [
+          { titre: 'Calculs de base', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' },
+          { titre: 'Fonctions usuelles', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Physique': [
+          { titre: 'Cinématique', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '10': {
+        'Info': [
+          { titre: 'Algorithmes', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Chimie': [
+          { titre: 'Réactions chimiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+  },
+
+  annales: {
+    '1A': {
+      '1': {
+        'Maths': [
+          { titre: 'Annale 2023', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '2': {
+        'Maths': [
+          { titre: 'Annale 2023', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '2A': {
+      '3': {
+        'Électrotechnique': [
+          { titre: 'Sujet RLC', visionner: 'https://drive.google.com/file/d/1gAn6hkQWSX2rBx6z1rYZF2oJ4zQe8ItF/preview', telecharger: 'https://drive.google.com/uc?export=download&id=1gAn6hkQWSX2rBx6z1rYZF2oJ4zQe8ItF' }
+        ]
+      },
+      '4': {
+        'Maths': [
+          { titre: 'Annale 2023', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '3A': {
+      '5': {
+        'Thermodynamique': [
+          { titre: 'Annale 2022', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Management': [
+          { titre: 'Sujet type QCM', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '6': {
+        'Maths': [
+          { titre: 'Annale 2023', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '4A': {
+      '7': {
+        'Thermodynamique': [
+          { titre: 'Annale 2022', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Management': [
+          { titre: 'Sujet type QCM', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '8': {
+        'Maths': [
+          { titre: 'Annale 2023', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '5A': {
+      '9': {
+        'Thermodynamique': [
+          { titre: 'Annale 2022', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Management': [
+          { titre: 'Sujet type QCM', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '10': {
+        'Maths': [
+          { titre: 'Annale 2023', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    }
+  },
+  cours: {
+    '1A': {
+      '1': {
+        'Maths': [
+          { titre: 'Calculs de base', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' },
+          { titre: 'Fonctions usuelles', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Physique': [
+          { titre: 'Cinématique', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '2': {
+        'Info': [
+          { titre: 'Algorithmes', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Chimie': [
+          { titre: 'Réactions chimiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '2A': {
+      '3': {
+        'Mécanique': [
+          { titre: 'Résistance des matériaux', visionner: 'https://drive.google.com/file/d/19i65g06_nhSZx5naNHAza6lH6VwfBbN2/preview', telecharger: 'https://drive.google.com/uc?export=download&id=19i65g06_nhSZx5naNHAza6lH6VwfBbN2' }
+        ],
+        'Maths': [
+          { titre: 'Intégrales', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '4': {
+        'Électrotechnique': [
+          { titre: 'Circuits RLC', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Informatique': [
+          { titre: 'Structures de données', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '3A': {
+      '5': {
+        'Électronique': [
+          { titre: 'Transistors', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Thermodynamique': [
+          { titre: 'Cycles thermodynamiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '6': {
+        'Management': [
+          { titre: 'Gestion de projet', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Innovation': [
+          { titre: 'Méthodes créatives', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '4A': {
+      '7': {
+        'Maths': [
+          { titre: 'Calculs de base', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' },
+          { titre: 'Fonctions usuelles', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Physique': [
+          { titre: 'Cinématique', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '8': {
+        'Info': [
+          { titre: 'Algorithmes', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Chimie': [
+          { titre: 'Réactions chimiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+    '5A': {
+      '9': {
+        'Maths': [
+          { titre: 'Calculs de base', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' },
+          { titre: 'Fonctions usuelles', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Physique': [
+          { titre: 'Cinématique', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      },
+      '10': {
+        'Info': [
+          { titre: 'Algorithmes', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ],
+        'Chimie': [
+          { titre: 'Réactions chimiques', visionner: 'mettez votre lien', telecharger: 'mettez votre lien' }
+        ]
+      }
+    },
+  }
+};
+
+
+let currentType = '';
+
+function updateAnnee() {
+  const typeSelect = document.getElementById("type");
+  const anneeSelect = document.getElementById("annee");
+  const semestreSelect = document.getElementById("semestre");
+  const matiereSelect = document.getElementById("matiere");
+  const container = document.getElementById("fiche-container");
+
+  currentType = typeSelect.value;
+  anneeSelect.innerHTML = '<option value="">-- Choisir une année --</option>';
+  semestreSelect.innerHTML = '<option value="">-- Choisir un semestre --</option>';
+  matiereSelect.innerHTML = '<option value="">-- Choisir une matière --</option>';
+  container.innerHTML = '';
+
+  semestreSelect.disabled = true;
+  matiereSelect.disabled = true;
+
+  const allAnnees = new Set();
+
+  ['fiches', 'annales', 'cours'].forEach(type => {
+    if (fichesData[type]) {
+      Object.keys(fichesData[type]).forEach(annee => {
+        allAnnees.add(annee);
+      });
+    }
+  });
+
+  allAnnees.forEach(annee => {
+    anneeSelect.innerHTML += `<option value="${annee}">${annee}</option>`;
+  });
+
+  anneeSelect.disabled = allAnnees.size === 0;
+}
+
+
+function updateSemestres() {
+  const annee = document.getElementById("annee").value;
+  const semestreSelect = document.getElementById("semestre");
+  const matiereSelect = document.getElementById("matiere");
+  const container = document.getElementById("fiche-container");
+
+  semestreSelect.innerHTML = '<option value="">-- Choisir un semestre --</option>';
+  matiereSelect.innerHTML = '<option value="">-- Choisir une matière --</option>';
+  container.innerHTML = '';
+  semestreSelect.disabled = true;
+  matiereSelect.disabled = true;
+
+  if (!annee) return;
+
+  const semestres = new Set();
+  ['fiches', 'annales', 'cours'].forEach(type => {
+    if (fichesData[type] && fichesData[type][annee]) {
+      Object.keys(fichesData[type][annee]).forEach(sem => {
+        semestres.add(sem);
+      });
+    }
+  });
+
+  semestres.forEach(sem => {
+    semestreSelect.innerHTML += `<option value="${sem}">Semestre ${sem}</option>`;
+  });
+
+  if (semestres.size > 0) semestreSelect.disabled = false;
+}
+
+function updateMatieres() {
+  const annee = document.getElementById("annee").value;
+  const semestre = document.getElementById("semestre").value;
+  const matiereSelect = document.getElementById("matiere");
+  const container = document.getElementById("fiche-container");
+
+  matiereSelect.innerHTML = '<option value="">-- Choisir une matière --</option>';
+  container.innerHTML = '';
+  matiereSelect.disabled = true;
+
+  if (annee && semestre) {
+    const typesToSearch = ['fiches', 'annales', 'cours'];
+    const matieresSet = new Set();
+
+    typesToSearch.forEach(type => {
+      if (fichesData[type] && fichesData[type][annee] && fichesData[type][annee][semestre]) {
+        Object.keys(fichesData[type][annee][semestre]).forEach(mat => {
+          matieresSet.add(mat);
+        });
+      }
+    });
+
+    matieresSet.forEach(mat => {
+      matiereSelect.innerHTML += `<option value="${mat}">${mat}</option>`;
+    });
+
+    if (matieresSet.size > 0) {
+      matiereSelect.disabled = false;
+    }
+  }
+}
+
+
+function updateFiches() {
+  const annee = document.getElementById("annee").value;
+  const semestre = document.getElementById("semestre").value;
+  const matiere = document.getElementById("matiere").value;
+  const selectedType = document.getElementById("type").value;
+  const container = document.getElementById("fiche-container");
+
+  container.innerHTML = '';
+
+  const typesToSearch = selectedType ? [selectedType] : ['fiches', 'annales', 'cours'];
+
+  typesToSearch.forEach(type => {
+    if (
+      fichesData[type] &&
+      fichesData[type][annee] &&
+      fichesData[type][annee][semestre] &&
+      fichesData[type][annee][semestre][matiere]
+    ) {
+      fichesData[type][annee][semestre][matiere].forEach(fiche => {
+        container.innerHTML += `
+          <div class="fiche-card">
+            <h3>📄 ${fiche.titre}</h3>
+            <div class="fiche-buttons">
+              <button class="btn" onclick="visionnerEnLigne('${fiche.visionner}')">👁️ Visionner</button>
+              <a href="${fiche.telecharger}" class="btn">⬇️ Télécharger</a>
+            </div>
+          </div>`;
+      });
+    }
+  });
+}
+
+
+function visionnerEnLigne(url) {
+  let iframe = document.getElementById('pdfViewer');
+  if (!iframe) {
+    iframe = document.createElement('iframe');
+    iframe.id = 'pdfViewer';
+    iframe.style.display = 'block';
+    iframe.style.width = '100%';
+    iframe.style.height = '600px';
+    iframe.style.marginTop = '1rem';
+    document.querySelector(".container").appendChild(iframe);
+  }
+  iframe.src = url;
+  iframe.scrollIntoView({ behavior: 'smooth' });
+}
+window.onload = function() {
+  updateAnnee();
+};
---
+++ script_ia.js
@@ -0,0 +1,304 @@
+const HUGGINGFACE_API_KEY = "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";
+const VOICERSS_API_KEY = "1d058c69a21947a19e5532b5aa657be7"; // Remplace ici
+
+const inputText = document.getElementById("inputText");
+const fileInput = document.getElementById("fileInput");
+const btnProcess = document.getElementById("btnProcess");
+const summaryOutput = document.getElementById("summaryOutput");
+const btnDownloadPdf = document.getElementById("btnDownloadPdf");
+const btnPlayAudio = document.getElementById("btnPlayAudio");
+const log = document.getElementById("log");
+const btnClearFile = document.getElementById("btnClearFile");
+
+// Création du bouton de téléchargement audio
+const btnDownloadAudio = document.getElementById("btnDownloadAudio");
+
+btnProcess.addEventListener("click", async () => {
+  log.textContent = "";
+  summaryOutput.value = "";
+
+  if (fileInput.files.length > 0) {
+    btnProcess.disabled = true;
+    const file = fileInput.files[0];
+    const ext = file.name.split(".").pop().toLowerCase();
+
+    try {
+      let text = "";
+      if (ext === "txt") {
+        text = await readTextFile(file);
+      } else if (ext === "pdf") {
+        text = await extractTextFromPDF(file);
+      } else {
+        alert("Format non supporté. Choisissez un fichier .txt ou .pdf");
+        btnProcess.disabled = false;
+        return;
+      }
+
+      log.textContent = `Texte extrait (${text.length} caractères). Envoi au résumé...`;
+      await summarizeAndDisplay(text);
+
+    } catch (e) {
+      log.textContent = "Erreur lors de la lecture du fichier : " + e.message;
+    } finally {
+      btnProcess.disabled = false;
+    }
+
+  } else {
+    const text = inputText.value.trim();
+    if (!text) {
+      alert("Veuillez saisir du texte ou sélectionner un fichier.");
+      return;
+    }
+
+    btnProcess.disabled = true;
+    log.textContent = "Envoi du texte saisi au résumé...";
+    try {
+      await summarizeAndDisplay(text);
+    } catch (e) {
+      log.textContent = "Erreur lors du résumé : " + e.message;
+    } finally {
+      btnProcess.disabled = false;
+    }
+  }
+});
+
+btnProcess2.addEventListener("click", async () => {
+  log.textContent = "";
+  summaryOutput.value = "";
+
+  if (fileInput.files.length > 0) {
+    btnProcess2.disabled = true;
+    const file = fileInput.files[0];
+    const ext = file.name.split(".").pop().toLowerCase();
+
+    try {
+      let text = "";
+      if (ext === "txt") {
+        text = await readTextFile(file);
+      } else if (ext === "pdf") {
+        text = await extractTextFromPDF(file);
+      } else {
+        alert("Format non supporté. Choisissez un fichier .txt ou .pdf");
+        btnProcess2.disabled = false;
+        return;
+      }
+
+      log.textContent = `Texte extrait (${text.length} caractères). Envoi au résumé...`;
+      await summarizeAndDisplay2(text);
+
+    } catch (e) {
+      log.textContent = "Erreur lors de la lecture du fichier : " + e.message;
+    } finally {
+      btnProcess2.disabled = false;
+    }
+
+  } else {
+    const text = inputText.value.trim();
+    if (!text) {
+      alert("Veuillez saisir du texte ou sélectionner un fichier.");
+      return;
+    }
+
+    btnProcess2.disabled = true;
+    log.textContent = "Envoi du texte saisi au résumé...";
+    try {
+      await summarizeAndDisplay2(text);
+    } catch (e) {
+      log.textContent = "Erreur lors du résumé : " + e.message;
+    } finally {
+      btnProcess2.disabled = false;
+    }
+  }
+});
+
+async function readTextFile(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = () => reject(new Error("Erreur lecture fichier"));
+    reader.readAsText(file);
+  });
+}
+
+async function extractTextFromPDF(file) {
+  const arrayBuffer = await file.arrayBuffer();
+  const pdf = await pdfjsLib.getDocument(new Uint8Array(arrayBuffer)).promise;
+
+  let fullText = "";
+  for (let i = 1; i <= pdf.numPages; i++) {
+    const page = await pdf.getPage(i);
+    const textContent = await page.getTextContent();
+    const pageText = textContent.items.map(item => item.str).join(" ");
+    fullText += pageText + "\n\n";
+  }
+  return fullText.trim();
+}
+
+async function summarizeAndDisplay(text) {
+  summaryOutput.value = "";
+  try {
+    const summary = await summarizeText(text);
+    summaryOutput.value = summary;
+    log.textContent = "Résumé généré avec succès.";
+  } catch (e) {
+    log.textContent = "Erreur lors du résumé : " + e.message;
+  }
+}
+
+async function summarizeAndDisplay2(text) {
+  summaryOutput.value = "";
+  try {
+    const summary = await summarizeText2(text);
+    summaryOutput.value = summary;
+    log.textContent = "Résumé généré avec succès.";
+  } catch (e) {
+    log.textContent = "Erreur lors du résumé : " + e.message;
+  }
+}
+
+async function summarizeText(text) {
+  const url = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6";
+        const textLength = text.length;
+const approxTokens = Math.floor(textLength / 4);
+
+const minLengthTokens = Math.max(Math.floor(approxTokens * 0.3), 20);  // minimum 20 tokens
+const maxLengthTokens = Math.min(Math.floor(approxTokens * 0.5), 300); // maximum 300 tokens
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      inputs: text,
+
+
+parameters: {
+  min_length: minLengthTokens,
+  max_length: maxLengthTokens,
+  do_sample: false
+}
+      // parameters: {
+      //  min_length: 120, // augmente la longueur minimale
+       // max_length: 300, // limite la longueur maximale
+      // do_sample: false // on garde un résumé déterministe
+     // } 
+    })
+  });
+
+  if (!response.ok) {
+    const err = await response.text();
+    throw new Error("Erreur API : " + err);
+  }
+
+  const data = await response.json();
+
+  if (Array.isArray(data) && data.length > 0 && data[0].summary_text) {
+    return data[0].summary_text;
+  } else {
+    throw new Error("Résumé non trouvé dans la réponse API.");
+  }
+}
+
+async function summarizeText2(text) {
+  const url = "https://api-inference.huggingface.co/models/sshleifer/distilbart-cnn-12-6";
+        const textLength = text.length;
+const approxTokens = Math.floor(textLength / 4);
+
+const minLengthTokens = Math.max(Math.floor(approxTokens * 0.1), 20);  // minimum 20 tokens
+const maxLengthTokens = Math.min(Math.floor(approxTokens * 0.2), 300); // maximum 300 tokens
+
+  const response = await fetch(url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${HUGGINGFACE_API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      inputs: text,
+
+
+parameters: {
+  min_length: minLengthTokens,
+  max_length: maxLengthTokens,
+  do_sample: false
+}
+      // parameters: {
+      //  min_length: 120, // augmente la longueur minimale
+       // max_length: 300, // limite la longueur maximale
+      // do_sample: false // on garde un résumé déterministe
+     // } 
+    })
+  });
+
+  if (!response.ok) {
+    const err = await response.text();
+    throw new Error("Erreur API : " + err);
+  }
+
+  const data = await response.json();
+
+  if (Array.isArray(data) && data.length > 0 && data[0].summary_text) {
+    return data[0].summary_text;
+  } else {
+    throw new Error("Résumé non trouvé dans la réponse API.");
+  }
+}
+
+btnDownloadPdf.addEventListener("click", () => {
+  const { jsPDF } = window.jspdf;
+  const doc = new jsPDF();
+
+  const text = summaryOutput.value.trim();
+  if (!text) {
+    alert("Aucun résumé à enregistrer.");
+    return;
+  }
+
+  const splitText = doc.splitTextToSize(text, 180);
+  doc.text(splitText, 10, 10);
+  doc.save("resume.pdf");
+});
+
+btnPlayAudio.addEventListener("click", () => {
+  const text = summaryOutput.value.trim();
+  if (!text) {
+    alert("Aucun résumé à lire.");
+    return;
+  }
+  const utterance = new SpeechSynthesisUtterance(text);
+  speechSynthesis.speak(utterance);
+});
+
+btnDownloadAudio.addEventListener("click", async () => {
+  const text = summaryOutput.value.trim();
+  if (!text) {
+    alert("Aucun résumé à convertir.");
+    return;
+  }
+
+  const url = `https://api.voicerss.org/?key=${VOICERSS_API_KEY}&hl=fr-fr&src=${encodeURIComponent(text)}&c=MP3`;
+
+  try {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const audioURL = URL.createObjectURL(blob);
+
+    const a = document.createElement("a");
+    a.href = audioURL;
+    a.download = "resume_audio.mp3";
+    a.click();
+
+  } catch (e) {
+    console.error("Erreur lors de la création du fichier audio :", e);
+    alert("Erreur lors de la génération du fichier audio.");
+  }
+});
+
+btnClearFile.addEventListener("click", () => {
+  fileInput.value = "";
+  summaryOutput.value = "";
+  log.textContent = "";
+  inputText.focus();
+});
